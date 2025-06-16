5f40a22ef3b42e8c65d50d93d63c84dd231fd248
Daryna Voloshyna <voloshyna.darina@gmail.com>
2025-06-16 21:01:07 +0300

    Task 1

---
+++ Task_1/main.py
@@ -0,0 +1,50 @@
+import datetime
+
+DATE_FORMAT = "%Y-%m-%d"
+
+def days_until(date: str):
+    today = datetime.datetime.today()
+    date = datetime.datetime.strptime(date, DATE_FORMAT)
+    return (today - date).days
+
+def get_valid_int(prompt: str, min_val: int, max_val: int):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value < min_val or value > max_val:
+                raise ValueError
+            return value
+        except ValueError:
+            print(f"Please enter a number between {min_val} and {max_val}.")
+
+def run():
+    print("Input desired date")
+    year = get_valid_int("Enter year (1–9999): ", min_val=1, max_val=9999)
+    month = get_valid_int("Enter month (1–12): ", min_val=1, max_val=12)
+
+    while True:
+        day = get_valid_int("Enter day (1–31): ", min_val=1, max_val=31)
+        try:
+            datetime_obj = datetime.date(year, month, day)
+            date_str = datetime_obj.strftime(DATE_FORMAT)
+            print(f"\nYou entered: {date_str} "
+                  f"\nTime until that date: {days_until(date_str)} days")
+            break
+        except ValueError:
+            print("Invalid day for the given month/year. Try again.")
+
+def test_days_until():
+    today = datetime.datetime.today().date()
+    future_date = today + datetime.timedelta(days=1)
+    past_date = today - datetime.timedelta(days=1)
+
+    days_to_future = days_until(future_date.strftime(DATE_FORMAT))
+    days_to_past = days_until(past_date.strftime(DATE_FORMAT))
+    assert days_to_future < 0, f"Expected negative timedelta, got {days_to_future}"
+    assert days_to_past > 0, f"Expected positive timedelta, got {days_to_past}"
+
+if __name__ == "__main__":
+    test_days_until()
+    print("✅ Test passed!")
+
+run()
