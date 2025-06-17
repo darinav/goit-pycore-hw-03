7eed879b0ddf2446b6b63fcf346fb9e9045de444
darinav <darinav@users.noreply.github.com>
2025-06-17 20:02:17 +0300

    Fix validation logic in get_numbers_ticket

--- Task_2/task_2.py
+++ Task_2/task_2.py
@@ -5,10 +5,17 @@ MAX_ALLOWED_NUMBER = 1000
 
 def get_numbers_ticket(min_val: int, max_val: int, quantity: int):
     try:
-        if (min_val < MIN_ALLOWED_NUMBER or max_val > MAX_ALLOWED_NUMBER
-                or quantity < min_val or quantity > max_val):
+        if (
+            min_val < MIN_ALLOWED_NUMBER
+            or max_val > MAX_ALLOWED_NUMBER
+            or min_val > max_val
+            or quantity < 1
+            or quantity > (max_val - min_val + 1)
+        ):
             raise ValueError
+
         return sorted(random.sample(range(min_val, max_val + 1), quantity))
+
     except ValueError:
         print("Wrong parameters.")
         return list()
